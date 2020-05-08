a55ca65875ee459936738f941fe895199c89abce
unknown <jl.crobeddu@codeur.online>
2020-05-08 17:45:31 +0200

    kill cookies if deconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -2,6 +2,7 @@
 session_start(); // INITIALISE SESSION
 session_unset(); // DESACTIVE SESSION   
 session_destroy(); // DESTROY SESSION
+setcookie('souvenir','',time()-1,'/',null,false,true);
 
 header('location: ./');
 exit();
--- src/cookie.php
+++ src/cookie.php
@@ -1,14 +1,13 @@
 <?php
-if (isset($_COOKIE['souvenir'])) {
+if (isset($_COOKIE['souvenir']) && !isset($_SESSION['connect'])) {
     require('src/connection_bdd.php'); 
-    
+    //RECUPERATION OF THE COOKIE NAME
     $valeur_cookie = htmlspecialchars($_COOKIE['souvenir']);
-   
+   //IS THERE SOMEONE IN DBB WITH THIS SECRET NUMBER
     $reqVal = $bdd->prepare('SELECT count(*) as nombre_res FROM user WHERE secret=?');
     $reqVal->execute(array($valeur_cookie));
 
     $resVal = $reqVal->fetch();
-    echo $resVal['nombre_res'];
     if ($resVal['nombre_res'] == 1) {
         $reqUser = $bdd->prepare('SELECT email,password FROM user WHERE secret=?');
         $reqUser->execute(array($valeur_cookie));
